050f6b973400d2bd67150019c41ee4fec061ed5e
argenych17 <kylychbekovargen14@gmail.com>
2022-06-24 23:02:41 +0600

    fech: instal  i18next

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useTranslation } from 'react-i18next';
+import { Trans, } from 'react-i18next';
+
 
 function App() {
+
+const {t , i18n} = useTranslation()
+
+const changeLanguage = (language) =>{
+  i18n.changeLanguage(language)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=> changeLanguage("en") } >EN</button>
+      <button onClick={()=> changeLanguage("ru") } >RU</button>
+      <hr/>
+      <Trans i18nKey="description.part1">
+      To get started , edit <code>src/App.js</code> and saveto reload
+        </Trans>
+        <div>{t("description.part2")}</div>
     </div>
   );
 }
