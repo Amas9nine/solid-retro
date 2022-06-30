9895585037121c6ba81ad5b84fac43c43bb7fef1
Akmaral <mdktva@gmail.com>
2022-06-30 21:52:20 +0600

    fix: redo route to homepage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/homePage/HomePage";
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+
         <Routes>
-          <Route path="/aboutUs" element={<h1>About us</h1>}/>
+          <Route path="/homePage" element={<HomePage />}/>
         </Routes>
 
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     </BrowserRouter>
   );
