673c9299b0e971a57ea52a8dd1741980b4a1cdda
Akmaral <mdktva@gmail.com>
2022-06-30 23:24:20 +0600

    fix: solved problems with linters

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/homePage/HomePage";
-
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-
-        <Routes>
-          <Route path="/homePage" element={<HomePage />}/>
-        </Routes>
-
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
