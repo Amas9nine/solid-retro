ca27c1525847820ee229de77574032bf19bb5857
Adilxa <adilhaimerov82@gmail.com>
2022-06-30 22:03:31 +0600

    feat: made folder and create scss files and finally test it

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import "./App.css";
-import { useTranslation } from 'react-i18next';
-import { Trans, } from 'react-i18next';
-import React from 'react'
-import {useSelector,useDispatch} from 'react-redux'
-import {helloWorld} from './reduxToolkit/reducer'
-
+import "../src/style/index.scss";
+import { useTranslation } from "react-i18next";
+import { Trans } from "react-i18next";
+import { useSelector, useDispatch } from "react-redux";
+import { helloWorld } from "./reduxToolkit/reducer";
 
 function App() {
-const {t , i18n} = useTranslation();
+  const { t, i18n } = useTranslation();
 
-const changeLanguage = (language) =>{
-  i18n.changeLanguage(language)
-}
-
-  const show = useSelector(state => state.toolkit.hello)
-  const dispatch = useDispatch()
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+  const show = useSelector((state) => state.toolkit.hello);
+  const dispatch = useDispatch();
   return (
     <>
-    <button onClick={()=> changeLanguage("en") } >EN</button>
-      <button onClick={()=> changeLanguage("ru") } >RU</button>
-      <hr/>
+      <div className="testContainer">
+        <p>he</p>
+        <p>hel</p>
+        <p>hell</p>
+        <p>hello</p>
+        <i>hello</i>
+      </div>
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("ru")}>RU</button>
+      <hr />
       <Trans i18nKey="description.part1">
-         To get started , edit <code>src/App.js</code> and saveto reload
-       </Trans>
-       <div>{t("description.part2")}</div>
-    {show} <button onClick = {()=>dispatch (helloWorld())}></button>
-</>
+        To get started , edit <code>src/App.js</code> and saveto reload
+      </Trans>
+      <div>{t("description.part2")}</div>
+      {show} <button onClick={() => dispatch(helloWorld())}></button>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './i18n'
-import { Provider } from 'react-redux';
-import { store } from './reduxToolkit';
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./i18n";
+import { Provider } from "react-redux";
+import { store } from "./reduxToolkit";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store = {store}>
-    <App />
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>
 );
