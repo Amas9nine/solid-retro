78bf09a05285cf1a5d09b83d27a7f27d274e9f42
Akmaral <mdktva@gmail.com>
2022-07-13 21:13:19 +0600

    feat: created register page

--- src/Components/registerPage/RegisterPage.jsx
+++ src/Components/registerPage/RegisterPage.jsx
@@ -1,29 +1,43 @@
 import css from "./RegisterPage.module.scss";
+import { useTranslation } from "react-i18next";
 export default function RegisterPage() {
+  const { t } = useTranslation();
   return (
     <div className={css.global}>
-      <h1>Easy Retro</h1>
+      <img className={css.easy_retro} src="./images/registerPage/easy_retro_logo.svg" alt="" />
       <div className={css.main}>
-        <h1>Register</h1>
-        <label htmlFor="">Name</label> <br />
-        <input type="text" placeholder="Name" /> <br />
-        <label htmlFor="Email">Email</label> <br />
-        <input type="email" placeholder="Your email" /> <br />
-        <label htmlFor="Email">Type your password</label> <br />
-        <input type="password" placeholder="Password" />
-        <p> Should contain at least 8 chars, 1 number, 1 uppercase and 1 lowercase. </p>
+        <h5>{t("register.register")}</h5>
+        <label htmlFor="">{t("register.name")}</label> <br />
+        <input type="text" placeholder={t("register.name")} /> <br />
+        <label htmlFor="Email">{t("register.email")} </label> <br />
+        <input type="email" placeholder={t("register.placeholder_email")} /> <br />
+        <label htmlFor="Email">{t("register.password")} </label> <br />
+        <input type="password" placeholder={t("register.placeholder_password")} />
+        <p> {t("register.p")}</p>
         <div className={css.checkbox}>
           <input type="checkbox" />
-          <span>By signing up you agree to our Terms and Privacy Policy</span> <br />
+          <span>
+            {t("register.checkbox")}
+            <a href=""> {t("register.terms")} </a>
+            {t("register.and")}
+            <a href=""> {t("register.privacy")} </a>
+          </span>
         </div>
         <div className={css.checkbox}>
           <input type="checkbox" />
-          <span>I want to receive updates, promotions and tips in my inbox </span>
+          <span>{t("register.checkbox2")} </span>
         </div>
         <br />
-        <button className={css.createAc}>Create account</button> <br />
-        <button className={css.signIn}>Sign in with Google</button> <br />
-        <h5>Already have an account? Login</h5>
+        <button className={css.createAc}>{t("register.create_account")}</button> <br />
+        <button className={css.signInBtn}>
+          <img src="./images/registerPage/google_icon.png" alt="" />
+          <span>{t("register.sign_in")}</span> <br />
+        </button>
+        <hr />
+        <span className={css.spanAccount}>
+          {t("register.account")}
+          <a href=""> {t("register.login")}</a>
+        </span>
       </div>
     </div>
   );
