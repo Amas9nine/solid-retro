bf6bb26a5e3957250acaa6ed1c82755ad681d840
Akmaral <mdktva@gmail.com>
2022-07-14 00:38:17 +0600

    fix: worked on comments

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,6 @@ import BottomCompanies from "../Components/bottomCompanies/BottomCompanies";
 import Benefits from "../Components/benefits/Benefits";
 import Companies from "../Components/companies/Companies";
 import Jumbotron from "../Components/jumbotron/Jumbotron";
-import Footer from "../../src/components/footer/Footer";
-import Benefits from "../Components/benefits/Benefits";
 
 export default function HomePage() {
   return (
