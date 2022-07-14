ff887a01d89731110aaa2250bc7c245b6f6e2557
Adilxa <adilhaimerov82@gmail.com>
2022-07-14 22:51:01 +0600

    feat: pricing layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage";
 import FeaturesPage from "./pages/FeaturesPage/FeaturesPage";
 import Footer from "./Components/footer/Footer";
 import Aboutpage from "./pages/AboutPage/AboutPage";
+import PricingPage from "./pages/PricingPage/PricingPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/features" element={<FeaturesPage />} />
         <Route path="/about" element={<Aboutpage />} />
+        <Route path="/pricing" element={<PricingPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/PricingPage/PricingPage.jsx
@@ -0,0 +1,72 @@
+import { useTranslation } from "react-i18next";
+import css from "./pricingPage.module.scss";
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import { pricing } from "../../constants/pricingPage";
+
+export default function PricingPage() {
+  const [head, setHead] = useState(false);
+  const { t } = useTranslation();
+  return (
+    <Container>
+      <div className={css.sectionHeader}>
+        <h1>{head ? <h1>{t("pricing.TryEasy")}</h1> : <h1>{t("pricing.TwoMounth")}</h1>}</h1>
+        <div className={css.toggleHeader}>
+          <span>{t("pricing.Showannual")}</span>
+          <div className={css.toggleBox}>
+            <span className={head ? css.active : ""} onClick={() => setHead(true)}>
+              {t("pricing.no")}
+            </span>
+            <span className={!head ? css.active : ""} onClick={() => setHead(false)}>
+              {t("pricing.yes")}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className={css.sectionMain}>
+        <div className={css.table}>
+          <div className={css.tableHeader}></div>
+          <div className={head ? css.first : css.activeFirst}></div>
+          <div className={css.second}>{t("pricing.priceList.publick")}</div>
+          <div className={css.thirth}>{t("pricing.priceList.number")}</div>
+          <div className={css.fourth}>{t("pricing.priceList.surveys")}</div>
+          <div className={css.fifth}>{t("pricing.priceList.unlimited")}</div>
+          <div className={css.sixth}>{t("pricing.priceList.unlimitedTeam")}</div>
+          <div className={css.seventh}></div>
+        </div>
+        {pricing.map((item) => (
+          <div className={item.popular ? css.popular : css.PriceTable}>
+            <div className={css.tableHeader}>{t(item.popular)}</div>
+            <div className={css.first}>
+              <span className={css.tag}>{t(item.whatFor)}</span>
+              <span style={{ fontWeight: 300, fontSize: 24 }}>
+                {t(head ? item.price : item.secPrice)}
+              </span>
+              <span>{t(item.per)}</span>
+              <p>{t(head ? "" : item.us)}</p>
+              <p>{t(head ? "" : "pricing.first.billed")}</p>
+            </div>
+            <div className={css.second}>{t(item.publick)}</div>
+            <div className={css.thirth}>{t(item.numberOfTeam)}</div>
+            <div className={css.fourth}>{t(item.surveys)}</div>
+            <div className={css.fifth}>
+              <span className={css.tag}>{t(item.unlimitedTeam)}</span>
+            </div>
+            <div className={css.sixth}>
+              <span className={css.tag}>{t(item.unlimitedTeamMem)}</span>
+            </div>
+            <div className={css.seventh}>
+              <button className={css.startBtn}>{t(item.start)}</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={css.contactUs}>
+        <span>
+          {t("pricing.forMore")}
+          <a href="">{t("pricing.contact")}</a>
+        </span>
+      </div>
+    </Container>
+  );
+}
