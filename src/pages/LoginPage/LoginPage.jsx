be8f99ecd20573c6c4224c7c3c40ef5c854c5b94
Akmaral <mdktva@gmail.com>
2022-07-20 00:16:34 +0600

    feat: login page layout

---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,38 @@
+import css from "./LoginPage.module.scss";
+import { useTranslation } from "react-i18next";
+export default function LoginPage() {
+  const { t } = useTranslation();
+  return (
+    <form className={css.global}>
+      <img
+        className={css.easy_retro}
+        src="./images/registerPage/easy_retro_logo.svg"
+        alt="easy retro logo"
+      />
+      <div className={css.main}>
+        <h5>{t("login.login")}</h5>
+        <label>
+          {t("login.email")}
+          <input type="email" placeholder={t("login.placeholder_email")} />
+        </label>
+        <label>
+          {t("login.password")}
+          <input type="password" placeholder={t("login.password")} />
+        </label>
+        <button className={css.loginBtn}>{t("login.login_btn")}</button>
+        <button className={css.signInBtn}>
+          <img src="./images/registerPage/google_icon.png" alt="google icon" />
+          <span>{t("login.sign_in_btn")}</span>
+        </button>
+        <div className={css.forgot_password}>
+          <a href="">{t("login.forgot_password")}</a>
+        </div>
+        <hr />
+        <div className={css.register}>
+          <span>{t("login.account")}</span>
+          <a href=""> {t("login.register")}</a>
+        </div>
+      </div>
+    </form>
+  );
+}
