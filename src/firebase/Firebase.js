b474871eade195950c8a980de78d05f3280900c9
Akmaral <mdktva@gmail.com>
2022-07-20 23:03:59 +0600

    feat: set up and config firebase

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+const firebaseConfig = {
+    apiKey: "AIzaSyC-cmrNEA9cyONQ8OY8X3kIiEseoK3i2eU",
+    authDomain: "easy-retro.firebaseapp.com",
+    projectId: "easy-retro",
+    storageBucket: "easy-retro.appspot.com",
+    messagingSenderId: "769494545713",
+    appId: "1:769494545713:web:21840468216e5706522c10"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  export {firebase}
