7cc9347ab3ac603e0cc739a3c47ad0e60ae7d0a6
Akmaral <mdktva@gmail.com>
2022-07-20 18:56:19 +0600

    fix: added Link

--- src/pages/PasswordResetPage/PasswordResetPage.jsx
+++ src/pages/PasswordResetPage/PasswordResetPage.jsx
@@ -1,10 +1,11 @@
 import css from "./PasswordResetPage.module.scss";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { Form } from "react-bootstrap";
 export default function PasswordResetPage() {
   const { t } = useTranslation();
   return (
-    <div className={css.global}>
+    <Form className={css.global}>
       <img
         className={css.easy_retro}
         src="./images/registerPage/easy_retro_logo.svg"
@@ -14,15 +15,15 @@ export default function PasswordResetPage() {
         <h5>{t("password.password_reset_title")}</h5>
         <label>
           {t("password.email")}
-          <input type="email" placeholder={t("password.email_placeholder")} />
+          <input type="email" placeholder={t("password.email_placeholder")} required />
         </label>
         <button className={css.reset_password_btn}>{t("password.reset_password_btn")}</button>
         <hr />
         <span className={css.back_to_login}>
           {t("password.or")}
-          <Link to="/login"> {t("password.back_to_login")}</Link>
+          <Link to="/"> {t("password.back_to_login")}</Link>
         </span>
       </div>
-    </div>
+    </Form>
   );
 }
